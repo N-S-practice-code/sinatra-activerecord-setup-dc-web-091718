ca1cd9cbc783ec61b2c1bd3618d4644c3a280b71
Flatiron School <flatironschool@Flatirons-MacBook-Air-2.local>
2018-10-09 14:33:41 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,12 @@
 ENV['SINATRA_ENV'] ||= "development"
 
+require 'sinatra/activerecord/rake'
+
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
+
+configure :development do
+  set :database, 'sqlite3:db/database.db'
+end
 
 require './app'
